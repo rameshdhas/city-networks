0f27701376044d8a18c58d46bbc780c59892cd32
Ramesh Dhason <jpaul@Rameshs-MBP.home>
2020-07-19 12:12:42 -0400

    Initial maven shell project

---
+++ src/main/java/com/city/networks/CityNetworksApplication.java
@@ -0,0 +1,13 @@
+package com.city.networks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityNetworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityNetworksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/city/networks/CityNetworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.city.networks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityNetworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
