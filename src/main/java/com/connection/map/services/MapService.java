8ac5d40be8002596f73a42377642488f178a923a
jpaul <jpaul@rameshs-mbp.home>
2020-07-20 12:11:45 -0400

    Added load data method

--- src/main/java/com/connection/map/services/MapService.java
+++ src/main/java/com/connection/map/services/MapService.java
@@ -15,6 +15,12 @@
  */
 package com.connection.map.services;
 
+import static com.connection.map.constants.MapConstants.COMMA;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -24,8 +30,14 @@ import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
 import com.connection.map.constants.Mode;
@@ -44,11 +56,17 @@ public class MapService {
 	
 	private Logger log = LoggerFactory.getLogger(MapService.class);
 	
+	@Autowired
+	private ResourceLoader resourceLoader;
+
+	@Value("${data.file.path}")
+	private String dataFile;
+	
 	private List<City> cities = new ArrayList<City>();
 	
 	private List<Connection> connections = new ArrayList<Connection>();
 
-	private Map<String, Set<String>> highwayMap = new HashMap<String, Set<String>>();
+	private Map<String, Set<String>> connectionsMap = new HashMap<String, Set<String>>();
 
 	public void addCity(City city) {
 		this.cities.add(city);
@@ -97,7 +115,7 @@ public class MapService {
 		
 		boolean isConnected = false;
 
-        if(highwayMap.containsKey(origin) && highwayMap.containsKey(destination)){
+        if(connectionsMap.containsKey(origin) && connectionsMap.containsKey(destination)){
             Set<String> citiesVisited = new HashSet<>();
             Queue<String> citiesToVisit = new LinkedList<>();
             citiesToVisit.add(origin);
@@ -106,7 +124,7 @@ public class MapService {
                 String city = citiesToVisit.poll();
                 isConnected = city.equalsIgnoreCase(destination);
                 
-                Set<String> neighbors = highwayMap.get(city);
+                Set<String> neighbors = connectionsMap.get(city);
 
                 for (String neighbor : neighbors) {
                     if (!citiesVisited.contains(neighbor)) {
@@ -124,23 +142,77 @@ public class MapService {
         return isConnected;
     }
 	
+	
+	@PostConstruct
+	public void loadData() {
+		if(log.isDebugEnabled()) {
+			log.debug("Entering loadData");
+		}
+		InputStream inputStream = null;
+		BufferedReader reader =  null;
+		try {
+			log.info("Loading data from "+ dataFile); 
+			Resource resource = resourceLoader.getResource(dataFile);
+			inputStream = resource.getInputStream();
+			reader =  new BufferedReader(new InputStreamReader(inputStream));
+		    
+			String line = null;
+	        while((line = reader.readLine()) != null){
+	        	String[] cities = line.split(COMMA);
+	            String firstCity = cities[0].trim();
+	            String secondCity = cities[1].trim();
+
+	            if(!connectionsMap.containsKey(firstCity))
+	            	connectionsMap.put(firstCity, new HashSet<String>());
+
+	            if(!connectionsMap.containsKey(secondCity))
+	            	connectionsMap.put(secondCity, new HashSet<String>());
+
+	            connectionsMap.get(firstCity).add(secondCity);
+	            connectionsMap.get(secondCity).add(firstCity);
+	        }
+	        
+	        log.info("Total cities loaded : "+ connectionsMap.size());
+		}
+		catch(Exception e) {
+			log.error("Error in loading data : "+ e.getMessage());
+		}
+		finally {
+			try {
+				if(reader != null) {
+					reader.close();
+				}
+				if(inputStream != null) {
+					inputStream.close();
+				}
+				
+			} catch (IOException e) {
+				log.error("Error in closing stream : "+ e.getMessage());
+			}
+		}
+		if(log.isDebugEnabled()) {
+			log.debug("Exiting loadData");
+		}
+	}
+	
+	
 	/**
-	 * Returns a list of supported city names.
+	 * Returns a set of supported city names.
 	 * 
-	 * @return List<String>
+	 * @return Set<String>
 	 */
-	public List<String> getAllCities(){
-		List<String> cities = new ArrayList<String>();
+	public Set<String> getAllCities(){
+		Set<String> cities = new HashSet<String>();
 		if(log.isDebugEnabled()) {
 			log.debug("Entering isConnected");
 		}
-		
-		
+		if(connectionsMap.size() > 0) {
+			cities = connectionsMap.keySet();
+		}
 		if(log.isDebugEnabled()) {
 			log.debug("Exiting isConnected");
 		}
 		return cities;
 	}
-	
 	
 }
