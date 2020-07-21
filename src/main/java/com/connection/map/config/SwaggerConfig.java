6e604b2ade3e108113afe63acd3489da11db8f95
Ramesh Dhason <drameshdhas@gmail.com>
2020-07-21 00:23:56 -0400

    Schema validation off

--- src/main/java/com/connection/map/config/SwaggerConfig.java
+++ src/main/java/com/connection/map/config/SwaggerConfig.java
@@ -28,6 +28,8 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -77,4 +79,10 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
 		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
+
+	@Bean
+	UiConfiguration uiConfig() {
+		return UiConfigurationBuilder.builder().displayRequestDuration(true).validatorUrl(null).build();
+	}
+
 }
