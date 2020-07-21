d07d49d4d2a122446603197eb5c3de824ebed692
Ramesh Dhason <drameshdhas@gmail.com>
2020-07-20 23:09:15 -0400

    Fixed swagger docs

--- src/main/java/com/connection/map/config/SwaggerConfig.java
+++ src/main/java/com/connection/map/config/SwaggerConfig.java
@@ -16,13 +16,16 @@
 
 package com.connection.map.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -39,6 +42,23 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig extends WebMvcConfigurationSupport {
 
+	@Value("${api.title}")
+	private String apiTitle;
+
+	@Value("${api.description}")
+	private String apiDescription;
+
+	@Value("${api.license}")
+	private String license;
+
+	@Value("${api.licenseurl}")
+	private String licenseUrl;
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title(apiTitle).description(apiDescription).license(license).licenseUrl(licenseUrl)
+				.build();
+	}
+
 	/**
 	 * Docket bean to set the component scan for rest controllers.
 	 * 
@@ -48,7 +68,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 	public Docket mapApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("com.connection.map.controllers")).paths(PathSelectors.any())
-				.build();
+				.build().apiInfo(apiInfo());
 
 	}
 
